bee020ba8ed1a9ee0ad09097303b375009d7c9ab
Blake Stevenson <blake@artex.dev>
2021-05-27 22:04:55 -0500

    Rewrite

--- src/main/java/me/blake/papiweb/PlaceholderWeb.java
+++ src/main/java/me/blake/papiweb/PlaceholderWeb.java
@@ -1,17 +1,93 @@
 package me.blake.papiweb;
 
+import com.google.gson.Gson;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
 
 public class PlaceholderWeb extends JavaPlugin {
 
-    private static final Logger log = Logger.getLogger("Minecraft");
+    private Javalin app = null;
 
     @Override
     public void onEnable() {
+        this.getCommand("placeholderweb").setExecutor(new PlaceholderWebCommand(this));
         this.saveDefaultConfig();
-        new ServerRunnable(this, this.getConfig().getInt("port")).runTaskAsynchronously(this);
+        setupWebServer();
+    }
+
+    @Override
+    public void onDisable() {
+        app.stop();
+    }
+
+    private void setupWebServer() {
+        // Get the current class loader.
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        // Temporarily set this thread's class loader to the plugin's class loader.
+        Thread.currentThread().setContextClassLoader(PlaceholderWeb.class.getClassLoader());
+
+        // Instantiate the web server (which will now load using the plugin's class loader).
+        app = Javalin.create().start(this.getConfig().getInt("port"));
+        app.get("/:player/:placeholder", ctx -> {
+            if(this.getConfig().getBoolean("debug")) {
+                this.getLogger().log(Level.INFO, ctx.ip() + ": " + ctx.method() + " " + ctx.fullUrl() + " " + ctx.status());
+            }
+            if(this.getConfig().getStringList("keys").contains(ctx.queryParam("key")) || !this.getConfig().getBoolean("auth")) {
+                ctx.contentType("application/json");
+                ctx.result(getPlaceholders(ctx));
+            } else {
+                ctx.status(401);
+                ctx.contentType("text/plain");
+                ctx.result("401 Unauthorized\n" + this.getDescription().getName() + " v" + this.getDescription().getVersion());
+            }
+        });
+        app.get("/*", ctx -> {
+            if(this.getConfig().getBoolean("debug")) {
+                this.getLogger().log(Level.INFO, ctx.ip() + ": " + ctx.method() + " " + ctx.fullUrl() + " " + ctx.status());
+            }
+            ctx.status(404);
+            ctx.result("404 Not Found\n" + this.getDescription().getName() + " v" + this.getDescription().getVersion());
+        });
+
+        // Put the original class loader back where it was.
+        Thread.currentThread().setContextClassLoader(classLoader);
+    }
+
+    private String getPlaceholders(Context ctx) {
+        Server server = this.getServer();
+        OfflinePlayer player;
+        if (ctx.pathParam("player").matches("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[34][0-9a-fA-F]{3}-[89ab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}")) {
+            player = server.getOfflinePlayer(UUID.fromString(ctx.pathParam("player")));
+        } else {
+            player = server.getOfflinePlayer(ctx.pathParam("player"));
+        }
+
+        Map<String, String> output = new HashMap<>();
+        if (ctx.pathParam("placeholder").contains(",")) {
+            String[] placeholders = ctx.pathParam("placeholder").split(",");
+            for (String p : placeholders) {
+                output.put(p, PlaceholderAPI.setPlaceholders(player, "%" + p + "%"));
+            }
+        } else {
+            String p = ctx.pathParam("placeholder");
+            output.put(p, PlaceholderAPI.setPlaceholders(player, "%" + p + "%"));
+        }
+        Gson gson = new Gson();
+        return gson.toJson(output);
+    }
+
+    public void restartSever() {
+        app.stop();
+        setupWebServer();
     }
 }
---
+++ src/main/java/me/blake/papiweb/PlaceholderWebCommand.java
@@ -0,0 +1,74 @@
+package me.blake.papiweb;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Level;
+
+public class PlaceholderWebCommand implements CommandExecutor {
+
+    private final PlaceholderWeb plugin;
+
+    public PlaceholderWebCommand(PlaceholderWeb pl) {
+       plugin = pl;
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(args.length > 0) {
+            if(args[0].equals("generate")) {
+                if (sender instanceof Player) {
+                    sender.sendMessage(ChatColor.RED + "[PlaceholderWeb] You may only generate new keys from the console!");
+                } else {
+                    UUID uuid = UUID.randomUUID();
+                    List<String> list = plugin.getConfig().getStringList("keys");
+                    list.add(uuid.toString());
+                    plugin.getConfig().set("keys", list);
+                    plugin.saveConfig();
+                    sender.sendMessage(ChatColor.GREEN + "[PlaceholderWeb] Key added: " + uuid);
+                    plugin.reloadConfig();
+                }
+                return true;
+            } else if (args[0].equals("reload")) {
+                plugin.reloadConfig();
+                plugin.restartSever();
+                sender.sendMessage(ChatColor.GREEN + "[PlaceholderWeb] Reloaded!");
+                return true;
+            } else if (args[0].equals("debug")) {
+                if (plugin.getConfig().getBoolean("debug")) {
+                    plugin.getConfig().set("debug", false);
+                    sender.sendMessage(ChatColor.YELLOW + "[PlaceholderWeb] Debug mode disabled!");
+                } else {
+                    plugin.getConfig().set("debug", true);
+                    sender.sendMessage(ChatColor.YELLOW + "[PlaceholderWeb] Debug mode enabled!");
+                }
+                plugin.saveConfig();
+                plugin.reloadConfig();
+                return true;
+            } else if(args.length == 3) {
+                if(args[0].equals("generateurl")) {
+                    InetAddress ip = null;
+                    try {
+                        ip = InetAddress.getLocalHost();
+                    } catch (UnknownHostException e) {
+                        plugin.getLogger().log(Level.SEVERE, "No server IP found!");
+                    }
+                    assert ip != null;
+                    sender.sendMessage(ChatColor.GREEN + "http://" + ip.getHostAddress() + ":" + plugin.getConfig().getInt("port") + "/" + args[1] + "/" + args[2]);
+                    return true;
+                }
+            } else if(args[0].equals("generateurl")) {
+                sender.sendMessage(ChatColor.RED + "Usage: /placeholderweb generateurl <player> <placeholders>");
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
